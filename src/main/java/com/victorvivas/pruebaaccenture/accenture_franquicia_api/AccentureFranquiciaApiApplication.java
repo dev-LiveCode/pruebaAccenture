ef4a29050c05a289613a784f8e5b9cfea5f01644
Live Code <vslivecode@gmail.com>
2025-07-12 11:49:22 -0500

    chore: commit inicial con la creación del proyecto

---
+++ src/main/java/com/victorvivas/pruebaaccenture/accenture_franquicia_api/AccentureFranquiciaApiApplication.java
@@ -0,0 +1,13 @@
+package com.victorvivas.pruebaaccenture.accenture_franquicia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureFranquiciaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentureFranquiciaApiApplication.class, args);
+	}
+
+}
